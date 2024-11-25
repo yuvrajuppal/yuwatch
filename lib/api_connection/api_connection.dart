0f6a1fae5be739d1ed64b551fcb8898ca2f831eb
yuvrajuppal <yuvrajuppal211@gmail.com>
2024-11-25 16:17:16 +0530

    windowsn runner fix

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,6 +1,6 @@
 class API{
   
-  static const hostconnect = "http://192.168.1.4/yu_4odtYXxiZD3cI1t_watch_php";
+  static const hostconnect = "http://192.168.1.7/yu_4odtYXxiZD3cI1t_watch_php";
   static const hostconnectuser = "$hostconnect/user";
 
 
